f6d96486f75941c753adb32993e9c290625d2ed7
rosso101 <victorprivv101@gmail.com>
2024-01-22 14:05:13 +0300

    added react

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div  class="p-3 mb-2 bg-warning text-dark">
+ <h1 className="d-flex justify-content-center">HEY!</h1>
+ <h1 className="d-flex justify-content-center">Looking for pizza?</h1>
+ <h1 className="d-flex justify-content-center">WE GOTCHU!</h1>
+ 
+    </div>
+    
+  );
+}
+
+export default App;
